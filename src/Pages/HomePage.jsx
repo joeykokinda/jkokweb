eea0b1b01045f581bd827129dfb5e3eb5ffc11f6
joey <jkokinda9@gmail.com>
2026-07-13 22:57:35 -0400

    Homepage: drop arrow from homelab link

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -77,7 +77,7 @@ function HomePage() {
             </p>
             <p className="home-sub home-setup-link">
               <Link to="/setup" className="text-link">
-                see my homelab →
+see my homelab
               </Link>
             </p>
           </div>
